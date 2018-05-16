897f6b0f4f3cea5b24ab63c5f0f6487e8d49ee2e
geekyzs <geekyzs@hotmail.com>
2018-05-16 19:16:00 +0800

    SB粉丝

--- components/amount-steps/index.js
+++ components/amount-steps/index.js
@@ -20,7 +20,7 @@ Component({
 
   methods: {
     handleTap: function (e) {
-      console.log(e);
+      //console.log(e);
       var myEventDetail = { step: e.target.dataset.step, index: e.target.dataset.index };// detail对象，提供给事件监听函数
       var myEventOption = {} // 触发事件的选项
       this.triggerEvent('action', myEventDetail, myEventOption);
