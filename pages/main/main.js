6b9ca7798a1602c864e7f8a6dc778b858917b827
muxiaolei <1484542698@qq.com>
2018-05-30 12:07:05 +0800

    推荐页面优化

--- pages/main/main.js
+++ pages/main/main.js
@@ -17,8 +17,10 @@ Page({
     indicatorDots: true,
     vertical: false,
     autoplay: true,
-    interval: 2000,
+    interval: 3000,
     duration: 500,
+    //采用衔接滑动
+    circular: true,
     shareit: false,
     reward: false
   },
