3b1f13f68e20fceec5f49b0d42facb6c88c43a0c
muxiaolei <1484542698@qq.com>
2018-06-11 19:44:18 +0800

    规则弹框修改

--- pages/valuation-rule/index.js
+++ pages/valuation-rule/index.js
@@ -5,7 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showRuleDialog: false
+    showRuleDialog: false,
+    tableRuleList: [{ content: '车主当日在线时长', isTitle: true}, 
+      { content: '车主当日广告收入', isTitle: true}, 
+      { content: '小于2小时' }, 
+      { content: '0元' }, 
+      { content: '大于等于2小时小于6小时' }, 
+      { content: '10元' }, 
+      { content: '大于等于6小时小于12小时' }, 
+      { content: '10元+每增加1小时多得3元' }, 
+      { content: '大于等于12小时' },
+      { content: '28元' }]
   },
 
   /**
