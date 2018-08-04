ac61b1d3d138715b55c290ce3b1001cce6284e30
muxiaolei <1484542698@qq.com>
2018-08-04 18:53:01 +0800

    我的任务

---
+++ pages/task/task.js
@@ -0,0 +1,26 @@
+// pages/task/task.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    task: {
+      finishList: [{
+        adLogo: '',
+        adName: '麦当劳麦当劳麦当劳麦当劳麦当劳麦当劳',
+        income: '565',
+        date: '07月12日-8月11日'
+      }]
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+})
